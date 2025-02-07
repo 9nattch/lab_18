6138f28be6fa9f5554fd1aa748fa57250d05a73a
9nattch <nattchayadao@gmail.com>
2025-02-08 00:11:17 +0700

    lab18_3.cpp completed

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,29 +1,107 @@
 #include<iostream>
+#include<string>
+#include<fstream>
+#include<vector>
+#include<cstdlib>
+#include<iomanip>
+
 using namespace std;
 
-struct Rect {
-    double x, y, w, h;  
+struct student{
+    string name;
+    int id;
+    char gender;
+    float gpa;
 };
 
+struct course{
+	string name;
+	int id;
+	vector<string> lecture_list;
+	vector<student *> student_list;
+};
 
-double overlap(Rect *R1, Rect *R2) {
-   
-    double R1_right = R1->x + R1->w;  
-    double R1_bottom = R1->y - R1->h; 
-    double R2_right = R2->x + R2->w;  
-    double R2_bottom = R2->y - R2->h; 
+student * findstudent(vector<student> &allstudents,int key){ //There is something wrong in this line.
+	for(unsigned int i = 0; i < allstudents.size(); i++){
+		if(allstudents[i].id  == key) return &allstudents[i];
+	}
+	return 0;
+}
 
-    double overlap_left = max(R1->x, R2->x);
-    double overlap_right = min(R1_right, R2_right);
-    double overlap_top = min(R1->y, R2->y);
-    double overlap_bottom = max(R1_bottom, R2_bottom);
+void printreport(vector<course> allcourses){
+	for(unsigned int i = 0;i < allcourses.size(); i++){
+		cout << "-----------------------------------------------------------------------------\n";
+		cout << left << setw(20);
+		cout << "Course:" << allcourses[i].name << " ("<< allcourses[i].id << ")\n\n";
+		cout << left << setw(20);
+		cout << "Lecturers:";
+		for(unsigned int j = 0; j < allcourses[i].lecture_list.size();j++){
+			if(j != 0) cout << ", ";
+			cout << allcourses[i].lecture_list[j];
+		}
+		cout << "\n\n";
+		cout << left << setw(20);
+		cout << "Students:";
+		for(unsigned int j = 0; j < allcourses[i].student_list.size();j++){
+			if(j != 0) cout << left << setw(20) << " ";
+			cout << left << setw(20) << allcourses[i].student_list[j]->name;
+			cout << left << setw(10) << allcourses[i].student_list[j]->id;
+			cout << left << setw(3) << allcourses[i].student_list[j]->gender;
+			cout << left << setw(5) << setprecision(2) << fixed << allcourses[i].student_list[j]->gpa << "\n";
+		} 
+		
+	}
+	cout << "-----------------------------------------------------------------------------\n";
+}
 
-    double overlap_w = overlap_right - overlap_left;
-    double overlap_h = overlap_top - overlap_bottom;
-
-    if (overlap_w <= 0 || overlap_h <= 0) {
-        return 0;
-    }
-
-    return overlap_w * overlap_h;
+int main(){
+	ifstream student_file("students.txt");
+	ifstream course_file("courses.txt");
+	vector<student> allstudents;
+	vector<course> allcourses;
+	
+	string textline;
+	
+	while(getline(student_file,textline)){
+		student s; 
+		stringstream ss(textline);
+		ss >> s.name >> s.id >> s.gender >> s.gpa;
+        char name[100];
+		sscanf(textline.c_str(),"%[^,],%d,%c,%f",name,&s.id,&s.gender,&s.gpa);
+	    s.name = name;
+		allstudents.push_back(s); 		
+	}
+	
+	int state = 1;
+	while(getline(course_file,textline)){
+		if(state == 1){
+			course c;
+			int loc = textline.find_first_of('(');
+			c.name = textline.substr(0,loc-1);
+			c.id = atof(textline.substr(loc+1,5).c_str());
+			getline(course_file,textline);
+			allcourses.push_back(c);
+			state = 2;			
+		}else if(state == 2){
+			if(textline == "> Students"){
+				state = 3;
+			}else{
+			    
+			    allcourses.back().lecture_list.push_back(textline);
+			}			
+		}else{
+			if(textline == "---------------------------------------"){
+				state = 1;
+			}else{
+				student *p = findstudent(allstudents,atof(textline.c_str()));
+				if(p != 0){
+				    allcourses.back().student_list.push_back(p);
+				}
+			
+				
+			}
+		}
+	}
+	printreport(allcourses);
+	
 }
